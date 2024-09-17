e29dd8a8049c5126238754eff4b39916456bfe16
root <root@localhost.localdomain>
2024-09-17 14:26:31 -0600

    agregado auxiliaturas

--- administrator/logs/joomla_scheduler.php
+++ administrator/logs/joomla_scheduler.php
@@ -16,3 +16,24 @@
 2024-09-12	22:43:26	INFO	Ha finalizado con éxito la tarea#03 en 0.51 (neto 0.51) segundos.
 2024-09-13	04:45:53	INFO	Ejecutando la tarea#03 'Update Notification'.
 2024-09-13	04:45:54	INFO	Ha finalizado con éxito la tarea#03 en 0.90 (neto 0.90) segundos.
+2024-09-15	12:58:33	INFO	Ejecutando la tarea#03 'Update Notification'.
+2024-09-15	12:58:34	INFO	Ha finalizado con éxito la tarea#03 en 0.91 (neto 0.91) segundos.
+2024-09-15	12:58:58	INFO	Ejecutando la tarea#02 'Session GC'.
+2024-09-15	12:58:58	INFO	Tarea> SessionGC end
+2024-09-15	12:58:58	INFO	Ha finalizado con éxito la tarea#02 en 0.00 (neto 0.00) segundos.
+2024-09-15	19:46:27	INFO	Ejecutando la tarea#03 'Update Notification'.
+2024-09-15	19:46:27	INFO	Ha finalizado con éxito la tarea#03 en 0.50 (neto 0.50) segundos.
+2024-09-16	02:27:58	INFO	Ejecutando la tarea#03 'Update Notification'.
+2024-09-16	02:27:59	INFO	Ha finalizado con éxito la tarea#03 en 0.81 (neto 0.81) segundos.
+2024-09-16	13:39:10	INFO	Ejecutando la tarea#03 'Update Notification'.
+2024-09-16	13:39:10	INFO	Ha finalizado con éxito la tarea#03 en 0.54 (neto 0.54) segundos.
+2024-09-16	13:44:10	INFO	Ejecutando la tarea#02 'Session GC'.
+2024-09-16	13:44:10	INFO	Tarea> SessionGC end
+2024-09-16	13:44:10	INFO	Ha finalizado con éxito la tarea#02 en 0.00 (neto 0.00) segundos.
+2024-09-16	21:08:57	INFO	Ejecutando la tarea#03 'Update Notification'.
+2024-09-16	21:08:57	INFO	Ha finalizado con éxito la tarea#03 en 0.46 (neto 0.46) segundos.
+2024-09-17	14:45:23	INFO	Ejecutando la tarea#03 'Update Notification'.
+2024-09-17	14:45:24	INFO	Ha finalizado con éxito la tarea#03 en 0.84 (neto 0.84) segundos.
+2024-09-17	14:45:29	INFO	Ejecutando la tarea#02 'Session GC'.
+2024-09-17	14:45:29	INFO	Tarea> SessionGC end
+2024-09-17	14:45:29	INFO	Ha finalizado con éxito la tarea#02 en 0.00 (neto 0.00) segundos.
